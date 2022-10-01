02b0417600d75105be9901cbbbfba254201af320
vinit patil <vinitpatil1001@gmail.com>
2022-10-01 15:04:17 +0530

    last push

--- api/settings.py
+++ api/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "hypekar1",
         "HOST": '127.0.0.1',
-        "PORT": 3306,
+        "PORT": 3307,
         "USER": 'root',
 
     }
